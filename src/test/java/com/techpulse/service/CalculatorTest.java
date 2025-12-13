b406cc5eeca12a55e302c64185864b481880da42
Gokul Falke <145555165+gokulfalke17@users.noreply.github.com>
2025-12-13 12:35:24 +0530

    TestCase : Changes the testcase to fail the jenkin test case

--- src/test/java/com/techpulse/service/CalculatorTest.java
+++ src/test/java/com/techpulse/service/CalculatorTest.java
@@ -11,7 +11,7 @@ public class CalculatorTest {
     @Test
     void testAdd() {
         int add = calculator.add(5, 10);
-        assertEquals(15, add);
+        assertEquals(20, add);
     }
 
     @Test
